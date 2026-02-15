18923be38ea569b90131889c9113a476586ed471
ap058175 <ap058175@gmail.com>
2026-02-15 15:34:26 +0530

    TestNg

--- src/test/java/TestNGLearning/Basic.java
+++ src/test/java/TestNGLearning/Basic.java
@@ -1,6 +1,7 @@
 package TestNGLearning;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 
@@ -21,6 +22,11 @@ public class Basic {
     @AfterMethod
     public void methodLast(){
         System.out.println("I am no 2 ,method in Basic class");
+    }
+    @Parameters ("URL")
+    @Test
+    public void loginpage(String urlname){
+        System.out.println("urlname");
     }
 
 }
